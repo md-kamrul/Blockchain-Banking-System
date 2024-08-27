5c236c1e1179f18151605454b7269a0310269d7a
Md. Kamrul Islam <join.kamrul@gmail.com>
2024-08-27 18:51:00 +0600

    Create login page

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,25 @@
 const Login = () => {
     return (
-        <div>
-            <h1>login page</h1>
+        <div className="bg-[#0C8CE9] h-screen w-full flex flex-col justify-center items-center gap-5">
+            <div>
+                <img src="../../public/logo.svg" alt="logo" className="w-[150px]" />
+            </div>
+            <div className="flex flex-col gap-3 w-[30%] mx-auto">
+                <div>
+                    <input type="text" placeholder="Username" className="input input-bordered w-full" />
+                </div>
+                <div>
+                    <input type="password" placeholder="Password" className="input input-bordered w-full" />
+                </div>
+                <div className="flex flex-col gap-3">
+                    <button className="btn w-full font-semibold text-[#2148C0]">LOGIN</button>
+                </div>
+                <div className="text-right text-white font-semibold">
+                    <h3>
+                        <a href="#">Forget Password</a>
+                    </h3>
+                </div>
+            </div>
         </div>
     );
 };
